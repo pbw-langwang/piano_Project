46b1acc451ca734c0ba454b67116bfa8e76972be
心异 <oncwnuHERwazove6bRwFEYcNYjnY@git.weixin.qq.com>
2021-02-06 14:48:19 +0800

    let change to const

--- dom.js
+++ dom.js
@@ -1,14 +1,14 @@
-let variable = {
-    bool: true,
-    pli: '',//不能只写一个pli，必须赋值，即使是空
-    array: [],
-}
-//es6的命名空间
+const variable = {
+        bool: true,
+        pli: '', //不能只写一个pli，必须赋值，即使是空
+        array: [],
+    }
+    //es6的命名空间
 
 function show1(element) {
-    let index = element.dataset.index;
-    let b = document.querySelectorAll(".head_music");
-    let a = b[index];
+    const index = element.dataset.index;
+    const b = document.querySelectorAll(".head_music");
+    const a = b[index];
     // 通过data-index来知道点击的是哪一个
     this.change(a, element);
 }
@@ -38,7 +38,7 @@ function drop(ev) {
     ev.stopPropagation();
     // 阻止浏览器对拖动的默认处理，但是firefox有点小bug
     // 加上ev.stopPropagation();已解决firefox的bug
-    var data = ev.dataTransfer.getData("Text");
+    const data = ev.dataTransfer.getData("Text");
     if (variable.bool) {
         ev.target.appendChild(document.getElementById(data));
         variable.bool = false;
@@ -49,8 +49,8 @@ function drop(ev) {
 function drop2(ev) {
     ev.preventDefault();
     ev.stopPropagation();
-    var data = ev.dataTransfer.getData("Text");
-    let data_id = ev.target.dataset.id;
+    const data = ev.dataTransfer.getData("Text");
+    const data_id = ev.target.dataset.id;
     if (!variable.bool && data === data_id) {
         // 通过id 和 data-id的对应关系，来确定是否可以移入
         ev.target.appendChild(document.getElementById(data));
@@ -60,10 +60,10 @@ function drop2(ev) {
 // 设置一个全局互斥变量，以防止多次拖动的bug
 
 function show2(element) {
-    let index2 = element.dataset.index2;
-    let b = document.querySelectorAll(".head_end_video");
-    let a = b[index2];
-    let c = a.parentElement.children[0].children[1];
+    const index2 = element.dataset.index2;
+    const b = document.querySelectorAll(".head_end_video");
+    const a = b[index2];
+    const c = a.parentElement.children[0].children[1];
     // c是获取到video父元素中的img
     this.change2(a, c);
 }
@@ -81,23 +81,23 @@ function change2(a, element) {
 
 
 function playaudio(event) {
-    let arr = document.querySelectorAll("audio");
-    if (variable.pli) {//把variable换成this也可以
+    const arr = document.querySelectorAll("audio");
+    if (variable.pli) { //把variable换成this也可以
         // 如果有变了颜色的，就改回其颜色
         variable.pli.style.setProperty("background-color", "white");
     }
-    for (i = 0; i < variable.array.length; i++) {
+    for (let i = 0; i < variable.array.length; i++) {
         // 判断最近5个中有没有按到的和正在按的相同，如果有就暂停并归零
         if (variable.array[i] === event.keyCode) {
-            for (j = 0; j < arr.length; j++) {
+            for (let j = 0; j < arr.length; j++) {
                 if (parseInt(arr[j].dataset.keynub) === event.keyCode) {
-                    arr[j].currentTime = 0;//设置时间为0，并暂停全部
+                    arr[j].currentTime = 0; //设置时间为0，并暂停全部
                     arr[i].pause();
                 }
             }
         }
     }
-    for (i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         if (parseInt(arr[i].dataset.keynub) === event.keyCode) {
             //通过keynub和keyCode的对应关系，判断哪一个播哦
             arr[i].play();
@@ -114,5 +114,3 @@ function playaudio(event) {
         }
     }
 }
-
-
